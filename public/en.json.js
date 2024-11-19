ce1f4e17b87e02ab9c2130024bedd04caeb496d3
Banayote <lenxrome@gmail.com>
2024-11-19 21:15:16 +0300

    Delete public directory

--- public/en.json.js
+++
@@ -1,7 +0,0 @@
-{
-    "storeName": "The Pink Closet",
-    "searchPlaceholder": "Search products...",
-    "createStore": "Create a Store",
-    "footerText": "&copy; The Pink Closet 2024. All rights reserved."
-}
-
--- public/es.json.js
+++
@@ -1,4 +0,0 @@
-{
-    "storeName": "El Armario Rosa",
-    "
-
