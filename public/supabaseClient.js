7f3b9edbe0c16ef7ca0213727dd1502c1811cb6d
Banayote <lenxrome@gmail.com>
2024-11-24 16:06:04 +0300

    Update supabaseClient.js

--- public/supabaseClient.js
+++ public/supabaseClient.js
@@ -1,9 +1,9 @@
 // Import the Supabase library
 import { createClient } from '@supabase/supabase-js';
 
-// Supabase URL and API key
-const SUPABASE_URL = 'https://eczpdpysryifkekieloc.supabase.co';
-const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjenBkcHlzcnlpZmtla2llbG9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwMjQ2NDUsImV4cCI6MjA0NzYwMDY0NX0.lxC6cqnwH9hMbGC4AsE5W6JRt34eyp4MxhNtAl0MdM4';
+// Use environment variables from .env file
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_API_KEY = process.env.NEXT_PUBLIC_SUPABASE_API_KEY;
 
 // Create the Supabase client
 const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
