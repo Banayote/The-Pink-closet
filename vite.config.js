45df78df56511a750a12b29dad9ac673e105fa20
Banayote <lenxrome@gmail.com>
2024-11-25 23:15:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'public', // Set the root directory to "public"
   build: {
+    outDir: '../dist', // Output the build files to "../dist"
+    assetsDir: 'assets', // Make sure assets are in the correct directory
     rollupOptions: {
       input: {
-        main: 'index.html',
-        store: 'store.html',
-        profile: 'profile.html',
-        messages: 'messages.html'
-      }
-    }
-  }
+        main: 'public/index.html', // Specify the main entry point
+      },
+    },
+  },
 });
-
