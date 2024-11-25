83ed958b54c07f89745f47583e9ace71be66dea1
Banayote <lenxrome@gmail.com>
2024-11-25 23:13:26 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'public', // Root directory containing all HTML files
   build: {
-    outDir: '../dist', // Where the built files will go
-  },
-  server: {
-    open: true, // Automatically open the app in the browser during dev
-  },
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        store: 'store.html',
+        profile: 'profile.html',
+        messages: 'messages.html'
+      }
+    }
+  }
 });
+
