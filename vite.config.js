5488b38176294bd4da3e98544dbd234bec360d03
Banayote <lenxrome@gmail.com>
2024-11-25 23:29:47 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: 'index.html',  // Ensure this points to the correct entry file
-    }
-  }
+      input: {
+        main: 'index.html', // Explicitly define the entry point as index.html
+      },
+    },
+    outDir: 'dist', // Ensure Vite places the built assets in the 'dist' directory
+  },
 });
