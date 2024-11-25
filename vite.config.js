56dd2a1ab2b31fa4199c0b9775848664e4aa1805
Banayote <lenxrome@gmail.com>
2024-11-25 23:21:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.', // Set the root directory to the project root
   build: {
-    outDir: './dist', // Output the build files to the "dist" directory inside the project root
-  },
+    rollupOptions: {
+      input: 'index.html',  // Ensure this points to the correct entry file
+    }
+  }
 });
