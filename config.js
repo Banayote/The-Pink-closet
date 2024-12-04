97e0afae69dda3b105c2ee877b24d374c024ca4a
Banayote <lenxrome@gmail.com>
2024-12-04 14:53:07 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+
+// Supabase Configuration
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+export const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
+
+// Cloudinary Configuration
+export const CLOUDINARY_URL = import.meta.env.VITE_CLOUDINARY_URL;
+export const CLOUDINARY_UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
